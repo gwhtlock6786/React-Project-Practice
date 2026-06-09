e434fc019b44af921f70e9a863e3a994f7a17c4c
Aaron Whitlock <gwhtlock@gmail.com>
2026-06-09 16:39:29 -0500

    style: refine header layout add stylying and  hover effects

---
+++ Week-1/Day-42/Lecture-Activities/food-store-app/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <h1>Food Store</h1>
+        <p>Owner: Gershon Whitlock</p>
+      </div>
+
+      <div className="header-center">
+        <p>Welcome to the Food Store!</p>
+      </div>
+
+      <div className="header-right">
+        <nav aria-label="Main-Navigation" class="main-nav">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#products">Products</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
