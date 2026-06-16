3b0e25d5daf7eed41b35fd603f9a9ce27046ca22
Aaron Whitlock <gwhtlock@gmail.com>
2026-06-16 11:34:37 -0500

    feat: add shift data and group shifts by day in ShiftDisplay

--- Week-1/Day-42/Lecture-Activities/food-store-app/src/App.jsx
+++ Week-1/Day-42/Lecture-Activities/food-store-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ShiftDisplay from './components/Shifts/ShiftDisplay/ShiftDisplay'
 import EmployeeList from './components/Employees/EmployeeList/EmployeeList'
 import MenuItemList from './components/MenuItems/MenuItemList/MenuItemList'
 import ProductList from './components/Products/ProductList/ProductList'
@@ -6,6 +7,8 @@ import Header from './components/Header/Header'
 import { employees } from './data/employee-data'
 import { menuItems } from './data/menu-item-data'
 import { productData } from './data/product-data'
+import { shiftData } from './data/shift-data'
+
 
 function App() {
  
@@ -16,6 +19,7 @@ function App() {
       <EmployeeList employees={employees}/>
       <MenuItemList menuItems={menuItems} />
       <ProductList products={productData} />
+      <ShiftDisplay shifts={shiftData} />
     </>
   )
 }
